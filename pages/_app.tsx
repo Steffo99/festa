61f11a3f0b8972ab6d12eea9d5d7c46414080d28
Stefano Pigozzi <me@steffo.eu>
2022-05-25 16:36:04 +0200

    Remove navbar imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css'
-import '../styles/nav.css'
 import '../styles/telegram.css'
 import '../styles/postcard.css'
 import type { AppProps } from 'next/app'
 import { LoginContext } from '../contexts/login'
 import { useState } from 'react'
 import * as Telegram from "../utils/telegram"
-import { Navbar } from '../components/Navbar'
 import defaultPostcard from "../images/adi-goldstein-Hli3R6LKibo-unsplash.jpg"
 import { Postcard } from '../components/Postcard'
 import { PostcardContext } from '../contexts/postcard'
