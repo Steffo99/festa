6fa2685bf11cef7cd378de7086fe5103a7e6c8aa
Stefano Pigozzi <me@steffo.eu>
2022-05-25 17:03:44 +0200

    Fix (again) i18n

---
+++ next-i18next.config.js
@@ -0,0 +1,11 @@
+const path = require("path")
+
+module.exports = {
+    i18n: {
+        defaultLocale: "it-IT",
+        locales: [
+            "it-IT",
+        ],
+    },
+    localePath: path.resolve('./public/locales'),
+}
--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-const path = require("path")
+const {i18n} = require("./next-i18next.config")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    defaultLocale: "it-IT",
-    locales: [
-      "it-IT",
-    ],
-    localePath: path.resolve('./public/locales'),
-  }
+  i18n,
 }
 
 module.exports = nextConfig
