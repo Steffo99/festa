1181564e9ebe2f5a93bd0df4250f9b4b39a3891a
Stefano Pigozzi <me@steffo.eu>
2022-05-31 16:35:13 +0200

    I keep forgetting to make smaller commits

---
+++ pages/api/events/mine.ts
@@ -0,0 +1,23 @@
+import { client } from "../../../utils/prismaClient";
+import { NextApiRequest, NextApiResponse } from "next";
+import { ApiResult } from "../../../types/api";
+import { restInPeace } from "../../../utils/restInPeace";
+import { handleInterrupts } from "../../../utils/interrupt";
+import { authorizeUser } from "../../../utils/apiAuth";
+import { Event } from "@prisma/client";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResult<Event | Event[]>>) {
+    handleInterrupts(res, async () => {
+        const user = await authorizeUser(req, res)
+
+        const where = {
+            creatorId: user.id
+        }
+    
+        await restInPeace(req, res, {
+            model: client.event,
+            list: {where}
+        })
+    })
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import { NextPageContext } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useState } from 'react';
 import { LoginContext } from '../contexts/login';
 import { useDefinedContext } from '../utils/definedContext';
-import { ApiError } from '../types/api';
-import { TelegramLoginButton } from "../components/TelegramLoginButton"
-import { useTelegramToFestaCallback } from '../hooks/useTelegramToFestaCallback';
+import { FormLoginTelegram } from '../components/FormLoginTelegram';
 
 
 export async function getStaticProps(context: NextPageContext) {
@@ -20,12 +17,9 @@ export async function getStaticProps(context: NextPageContext) {
 
 export default function PageIndex() {
     const { t } = useTranslation("common")
-    const [login, setLogin] = useDefinedContext(LoginContext)
-    const [working, setWorking] = useState<boolean>(false)
-    const [error, setError] = useState<ApiError | null | undefined>(null)
-    const onLogin = useTelegramToFestaCallback(setLogin, setError, setWorking)
-    
-    if(!login) return (
+    const [login, _] = useDefinedContext(LoginContext)
+
+    return (
         <main id="page-index" className="page">
             <hgroup className="hero-titles">
                 <h1>
@@ -35,69 +29,35 @@ export default function PageIndex() {
                     {t("siteSubtitle")}
                 </h2>
             </hgroup>
-            {error ? 
-                <div className="hero-action negative">
-                    <p>
-                        {t("telegramLoginError")}
-                    </p>
-                    <p>
-                        <code>
-                            {JSON.stringify(error)}
-                        </code>
-                    </p>
-                </div>
-                :
-                working ?
-                    <div>
+            {login ?
+                <div className="hero-action">
+                    <label htmlFor="input-name">
                         <p>
-                            {t("telegramLoginWorking")}
+                            {t("eventsInputDescriptionFirst")}
                         </p>
-                    </div>
-                    :
-                    <div className="hero-action">
-                        <p>
-                            {t("telegramLoginDescription")}
-                        </p>
-                        <TelegramLoginButton
-                            dataOnauth={onLogin}
-                            botName={process.env.NEXT_PUBLIC_TELEGRAM_USERNAME}
+                    </label>
+                    <form 
+                        className="form-event-create"
+                    >
+                        <input 
+                            id="input-name" 
+                            placeholder={t("eventsInputName")}
+                            type="text"
                         />
-                    </div>
+                        <input
+                            id="input-submit"
+                            type="submit"
+                            aria-label={t("eventsInputSubmitLabel")}
+                            value="→"
+                            className="square-40 positive"
+                        />
+                    </form>
+                </div>
+            :
+                <FormLoginTelegram
+                    className="hero-action"
+                />
             }
-        </main>
-    )
-
-    return (
-        <main id="page-index" className="page">
-            <hgroup className="hero-titles">
-                <h1>
-                    {t("siteTitle")}
-                </h1>
-                <h2>
-                    {t("eventsSubtitleFirst")}
-                </h2>
-            </hgroup>
-            <div className="hero-action">
-                <label htmlFor="input-name">
-                    <p>
-                        {t("eventsInputDescriptionFirst")}
-                    </p>
-                </label>
-                <form className="form-event-create">
-                    <input 
-                        id="input-name" 
-                        placeholder={t("eventsInputName")}
-                        type="text"
-                    />
-                    <input
-                        id="input-submit"
-                        type="submit"
-                        aria-label={t("eventsInputSubmitLabel")}
-                        value="→"
-                        className="square-40 positive"
-                    />
-                </form>
-            </div>
         </main>
     )
 }
