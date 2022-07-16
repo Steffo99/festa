b8319beda4453810f07cbb70b323470c379111f4
Stefano Pigozzi <me@steffo.eu>
2022-07-16 18:10:31 +0200

    Move icon styling to CSS module

--- components/generic/renderers/fontawesome.tsx
+++ components/generic/renderers/fontawesome.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
 import { default as classNames } from "classnames";
 import { memo } from "react";
+import style from "./fontawesome.module.css"
 
 /**
  * Component which adds proper styling to {@link FontAwesomeIcon}.
@@ -9,7 +10,7 @@ export const FestaIcon = memo((props: FontAwesomeIconProps) => {
     return (
         <FontAwesomeIcon
             {...props}
-            className={classNames("icon", props.className)}
+            className={classNames(style.festaIcon, props.className)}
         />
     )
 })
