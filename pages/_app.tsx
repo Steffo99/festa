e6e667a1d3b0c98197004dd14841b8189364b6c3
Stefano Pigozzi <me@steffo.eu>
2022-07-17 02:57:02 +0200

    Clean up imports in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,15 @@
+import '../styles/globals.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import { AppProps } from 'next/app'
 import { appWithTranslation, useTranslation } from 'next-i18next'
 import { AxiosSWRFetcherProvider } from '../components/auth/requests'
 import { useStatePostcard } from '../components/postcard/storage'
 import { PageErrorBoundary } from '../components/generic/errors/boundaries'
 import { PostcardContext } from '../components/postcard/base'
-import { AuthContext, AuthContextProvider } from '../components/auth/base'
+import { AuthContextProvider } from '../components/auth/base'
 import { PostcardRenderer } from '../components/postcard/renderer'
-import '../styles/globals.css'
-import defaultPostcard from "../public/postcards/adi-goldstein-Hli3R6LKibo-unsplash.jpg"
 import { config as fontAwesomeConfig } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import { useLocalStorageAuthState } from '../components/auth/storage'
+import defaultPostcard from "../public/postcards/adi-goldstein-Hli3R6LKibo-unsplash.jpg"
 
 
 fontAwesomeConfig.autoAddCss = false
