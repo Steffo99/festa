8e5221814d40d70ca417b179a24cef6ba493eaec
Stefano Pigozzi <me@steffo.eu>
2022-07-18 06:09:51 +0200

    Fix typescript doing something weird again

--- components/generic/editable/provider.tsx
+++ components/generic/editable/provider.tsx
@@ -5,7 +5,7 @@ import { EditingContext, EditingMode } from "./base"
 /**
  * Component which stores the current editing mode (by default {@link EditingMode.VIEW}) using {@link useState} and provides it to its children through an {@link EditingContext}.
  */
-export const EditingModeProvider = ({ children }: { children: ReactNode }) => {
+export const EditingContextProvider = ({ children }: { children: ReactNode }): JSX.Element => {
     return (
         <EditingContext.Provider value={useState<EditingMode>(EditingMode.VIEW)}>
             {children}
